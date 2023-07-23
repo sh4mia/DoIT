2ad74518456cb77363194bad912a52354c702e02
sh4mia <sobalkowski@gmail.com>
2023-07-23 20:51:28 +0200

    fixed profile update form (error when submitting)

--- friend_system/forms.py
+++ friend_system/forms.py
@@ -2,8 +2,9 @@ from django import forms
 from .models import UserProfile
 
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField(widget=forms.EmailInput, label='E-mail:', required=False)
-
+    email = forms.EmailField(widget=forms.EmailInput, label='E-mail:', required=True)
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
     class Meta:
         model = UserProfile
         fields = ['email', 'first_name', 'last_name']
